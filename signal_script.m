3ba30ac3f0685fb8987b9be5ee019d9108d6d219
Vitthal Maledar <mmvitmal41@gmail.com>
2020-12-07 11:34:49 +0530

    the script is written for the generation of bumpy road disturbance.

--- signal_script.m
+++ signal_script.m
@@ -3,4 +3,5 @@ t=[linspace(0,pi,6),linspace(pi,2*pi,6),linspace(2*pi,3*pi,6),linspace(3*pi,4*pi
 r=[0.2*sin(t(1:6)),0*sin(t(7:12)),0.2*sin(t(13:18)),0*sin(t(19:24)),0.2*sin(t(25:30))];
 t=t';
 r=r';
+%dataset for generating the signal%
 signal= timeseries(r,t); 
