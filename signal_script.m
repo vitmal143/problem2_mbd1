7704b11bf756ca728dc0685dcace396a1079c1be
Vitthal Maledar <mmvitmal41@gmail.com>
2020-12-07 11:25:25 +0530

    started with modelling the given the system and created the bumpy road signal.

---
+++ signal_script.m
@@ -0,0 +1,6 @@
+%signal generation of bumpy road%
+t=[linspace(0,pi,6),linspace(pi,2*pi,6),linspace(2*pi,3*pi,6),linspace(3*pi,4*pi,6),linspace(4*pi,5*pi,6)];
+r=[0.2*sin(t(1:6)),0*sin(t(7:12)),0.2*sin(t(13:18)),0*sin(t(19:24)),0.2*sin(t(25:30))];
+t=t';
+r=r';
+signal= timeseries(r,t); 
